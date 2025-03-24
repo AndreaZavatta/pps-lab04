1571e0b6e03911de5f70e233657ee9b13dc1334b
Andrea Zavatta <zavattaandrea@gmail.com>
2025-03-24 14:03:12 +0100

    complete exercise

--- src/main/scala/u03/extensionmethods/Sequences.scala
+++ src/main/scala/u03/extensionmethods/Sequences.scala
@@ -1,5 +1,7 @@
 package u03.extensionmethods
 
+import scala.annotation.tailrec
+
 object Sequences:
   
   enum Sequence[E]:
@@ -34,6 +36,9 @@ object Sequences:
         case Cons(h, t) => Cons(h, t.concat(other))
         case Nil()      => other
 
+      def distinct(): Sequence[A] = l match
+        case Cons(h,t) => Cons(h, t.filter(_ != h).distinct())
+        case Nil()     => Nil()
 
     def of[A](n: Int, a: A): Sequence[A] =
       if (n == 0) then Nil[A]() else Cons(a, of(n - 1, a))
